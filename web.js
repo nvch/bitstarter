8880d53c4e24b9fa7360f3727b52d0b7bd500e88
John Smith <example@stanford.edu>
2013-07-10 18:35:30 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+buf = new Buffer(256);
+buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send('new test');
 });
 
 var port = process.env.PORT || 5000;
